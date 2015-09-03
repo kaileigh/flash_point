2aeb2f225e2fabecbb97b3f340fd79c2f68f3473
Kaileigh Ahlquist <kaileigh.ahlquist@gmail.com>
2015-09-02 20:15:44 -0400

    Added argument check
    Added check to make sure that there are three arguments

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import sys
 import board_utils
-
+if len(sys.argv) != 3:
+	print "Usage: main.py board_name num_rows num_cols"
+	sys.exit(0)
 board = board_utils.create_board(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
-print sys.argv[1]
